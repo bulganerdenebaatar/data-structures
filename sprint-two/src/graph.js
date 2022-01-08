b78aea2773938c1ed3401cde5444027d4f9252e6
Benjue Han <vivianhan0620@gmail.com>
2022-01-07 19:49:38 -0800

    finish thru set

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,145 @@
+/*
+example nodes:
+nodes:[1      ,2      ,3      ,4      ]
+edges:[[2,3]  ,[1,3]  ,[1,2]  ,[3]    ]
 
-
+- add: 1) push to the end of nodes
+       2) push an empty arr to edges;
+       * this is making sure the node and its edges are at the same index
+- contain: check if nodes arr has input node
+- remove: 1) find the node's index,
+          2) nodes.splice(index,1)
+          3) edges.splice(index,1)
+*/
 // Instantiate a new graph
 var Graph = function() {
+// edges - ponters
+// nodes - vertices
 };
 
+//{node:edges}
+//{1: [2, 3],
+// 2: [1, 3]}
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  // this.nodes.push(node);
+  // console.log(node);
+  //is there a way to add a new property with input node as key??
+  this[node] = [];
+  return this;
+  // this.node = node;
+  // this.node = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  // while (this.nodes.length > 0) {
+  //   for (var i = 0; i < this.nodes.length; i ++) {
+  //     if (this.nodes[i] === node) {
+  //       return true;
+  //     }
+  //     return false;
+  //   }
+  // }
+  // return false;
+  /* iterate over the key in this, if any key is input node, return true, else return false
+  */
+  for (var key in this) {
+    //key is a string, needed to be converted to a number to match the node type
+    if (Number(key) === node) {
+      // console.log(key === node);
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  // delete node
+  // also delete that node from the edges // iterate through edges
+  // var edgeToBeDeleted;
+  // console.log(this);
+  for (var key in this) {
+    var eachEdges = this[key];
+    if (Number(key) === node) {
+      // console.log(this[key]);
+      delete this[key];
+    }
+    for (var i = 0; i < eachEdges.length; i ++) {
+      if (eachEdges[i] === node) {
+        eachEdges.splice(i, 1);
+      }
+    }
+  }
+  return this;
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  // console.log(fromNode);
+  //go to fromNode's edges, check if toNode is there
+  var fromNodeEdges = this[fromNode];
+  if (fromNodeEdges.length === 0) {
+    return false;
+  }
+  for (var i = 0; i < fromNodeEdges.length; i++) {
+    if (fromNodeEdges[i] === toNode) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  //go to the fromNode's edges, push toNode
+  //go to toNode's edges, push fromNode
+  // console.log(toNode, fromNode);
+  this[toNode].push(fromNode);
+  this[fromNode].push(toNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var fromNodeEdges = this[fromNode];
+  var toNodeEdges = this[toNode];
+  for (var i = 0; i < fromNodeEdges.length; i++) {
+    if (fromNodeEdges[i] === toNode) {
+      // console.log(fromNodeEdges);
+      fromNodeEdges.splice(i, 1);
+      // console.log(fromNodeEdges);
+    }
+  }
+  for (var i = 0; i < toNodeEdges.length; i++) {
+    if (toNodeEdges[i] === fromNode) {
+      // console.log(toNodeEdges);
+      toNodeEdges.splice(i, 1);
+      // console.log(toNodeEdges);
+    }
+  }
+  return this;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  // console.log(this);
+  for (var key in this) {
+    // console.log(key);
+    // console.log(typeof this[key]);
+    if (Array.isArray(this[key])) {
+      cb(key);
+    }
+  }
+  // while (Array.isArray(value) ) {
+  //   console.log(this[key]);
+  //   cb(key);
+  // }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
-
+var newGraph = new Graph();
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,73 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //if the list is empty, assign both value to head and tail as starting point
+    // create a new node(value)
+    // console.log('add to tail list', list);
+    var newNode = new Node(value);
+    if (list.head === null || list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail = newNode;
+      list.head.next = list.tail;
+    }
+    // [[val       ,next]]
+    //  head, tail
+    //  [[val ,next][val        ,next]]
+    //  head,  ------>   , tail , null
+    // head.value = new value - value
   };
 
   list.removeHead = function() {
+    var removed = list.head.value;
+    list.head = list.head.next;
+    // delete removed;
+    return removed;
   };
 
   list.contains = function(target) {
+    // while tail.next !== null
+    // list.head.value === target
+    // yes return true
+    // if no then list.head.next === target
+
+    /*tail.next will always be null,
+    so need to check if node.next is null
+    if currentnode.next is null, stop iterating
+
+    */
+    var currentNode = list.head;
+    // console.log(currentNode);
+
+    if (!currentNode.next && currentNode.value === target) {
+      return true;
+    } else if (!currentNode.next && currentNode.value !== target) {
+      return false;
+    }
+
+    //if nextnode is defined, do a while loop to check the currentnodes value
+    while (currentNode.next) {
+      if (currentNode.value === target || currentNode.next.value === target) {
+        currentNode = currentNode.next;
+        return true;
+      } else {
+        return false;
+      }
+    }
+    // return false;
+
+    //** tail.next is always null
+    // while (list.tail.next !== null) {
+    //   if (list.head.value === target) {
+    //     return true;
+    //   } else {
+    //     if (list.head.next.value === target) {
+    //       return true;
+    //     }
+    // list.head = list.head.next;
+    // list.head = next.value
+
   };
 
   return list;
